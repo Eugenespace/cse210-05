98d292ab40873894c6ef322056093001ae1b8fd5
owamamwen1 <87833174+owas1@users.noreply.github.com>
2022-02-11 08:16:05 +0100

    word

--- game/word_handler.py
+++ game/word_handler.py
@@ -19,37 +19,13 @@ class Word_handler:
     def guess_true(guess, chosenword, blanks):
         # parse the array and change the blank to corresponding letter location
         # return updated blank array
-        '''EXAMPLE -
-        for i in range (len(text_list)):
-        if text_list [i] == 'AB':
-            print (text_list [i])
-            text_list [i] = "Alberta"
-        count = text_list.count ("Alberta")
 
-        print (text_list)
-        Wayback Machine
-        '''
         for i in range(len(chosenword)):
             if chosenword[i] == guess:
                 blanks[i] = guess
 
         return blanks
 
-        ''' 
-        
-        print(guess+"1")
-        for guess in range(len(chosenword)):
-            print(guess+"2")
-            if chosenword[guess] == guess:
-                print(guess+"3")
-                #blanks[i] = guess
-                
-
-        
-        return guess
-        
-
     def guess_false():
         #
         pass
-'''
