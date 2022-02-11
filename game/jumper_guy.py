a5110b19768a4863e4f4b9c3d87c66e02ed2eab1
owamamwen1 <87833174+owas1@users.noreply.github.com>
2022-02-11 10:07:01 +0100

    jumper console print image

--- game/jumper_guy.py
+++ game/jumper_guy.py
@@ -5,12 +5,23 @@ class Jumper:
 
     def __init__(self):
         """Constructs jumper parachute"""
-        self._jumperParachute = [" _____", "/_____\\",
-                                 "\\     /", " \\   /", "   0 ", "  /|\\", "  / \\"]
+        # self._jumperParachute = [" _____", "/_____\\",
+        #                          "\\     /", " \\   /", "   0 ", "  /|\\", "  / \\"]
+        j1 = (f"\n   _____  ")
+        j2 = (f"  /_____\ ")
+        j3 = (f"  \     /")
+        j4 = (f"   \   / ")
+        j5 = (f"     O   ")
+        j6 = (f"    /|\  ")
+        j7 = (f"    / \  ")
+        j8 = (f"\n^ ^ ^ ^ ^ ^ ^")
+        self._jumperParachute = [j1, j2, j3, j4, j5, j6, j7, j8]
+        for i in range(len(self._jumperParachute)):
+            print(self._jumperParachute[i])
 
     def get_parachute(self):
         """creates a parachute array"""
-        return self._jumperParachute
+        # return self._jumperParachute
 
     def untie_parachute(self, guess):
         """If guess is right the parachute remains intact if not untie the parachute"""
